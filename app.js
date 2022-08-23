1f1ab8acc56a9b7fbbf21daa05a635752754512b
oladimeji <tommyslipz@gmail.com>
2022-08-23 07:20:27 +0100

    make it better

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
 app.use(express.json());
 
 // custom middleware

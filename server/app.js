27b8ed95074a044d699cff3c56059a730a32b500
oladimeji <tommyslipz@gmail.com>
2022-09-09 16:33:51 +0100

    make it better

--- server/app.js
+++ server/app.js
@@ -23,7 +23,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/client/build'));
 
   app.get('*', (req, res) => {
-    let filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
+    let filePath = path.resolve(
+      __dirname,
+      '/app/server/client/build/',
+      'index.html'
+    );
     console.log(filePath), 'Filepath';
     res.sendFile(filePath);
   });

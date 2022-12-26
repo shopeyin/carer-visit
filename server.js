3f192ae436c98f9f76649e67c7d6eb53a770d489
oladimeji <tommyslipz@gmail.com>
2022-12-26 08:32:41 +0000

    changes

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ dotenv.config({
   path: './config.env',
 });
 const app = require('./app');
-console.log(process.env.DATABASE_PASSWORD);
-console.log(process.env.DATABASE);
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD

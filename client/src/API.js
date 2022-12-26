69c56f488d364e7914ba3c268d7b132c497dca5c
oladimeji <tommyslipz@gmail.com>
2022-12-26 15:47:30 +0000

    changes

--- client/src/API.js
+++ client/src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/api/v1/',
+  baseURL: 'https://carer-visit.herokuapp.com/api/v1/',
   headers: {
     'Content-type': 'application/json',
   },

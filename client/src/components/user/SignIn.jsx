fa10838eafcfb75d355a8f14257d41981a9ebdc8
oladimeji <tommyslipz@gmail.com>
2023-01-03 08:25:39 +0000

    changes to the admin page

--- client/src/components/user/SignIn.jsx
+++ client/src/components/user/SignIn.jsx
@@ -37,7 +37,7 @@ function SignIn({ login }) {
     return (
       <div className="container-fluid signin__container">
         <div className="row signin__container-form">
-          <div className="col-11 col-sm-6 col-md-4 m-3">
+          <div className="col-11 col-sm-6 col-md-6 m-3">
             <form onSubmit={handleSubmit}>
               <p className="error-msg">
                 {' '}

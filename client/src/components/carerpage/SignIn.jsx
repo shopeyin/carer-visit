1713dbf3af29e4e94b7d685ff32d03d2d217ff43
oladimeji <tommyslipz@gmail.com>
2023-01-10 07:55:35 +0000

    added github actions

--- client/src/components/carerpage/SignIn.jsx
+++ client/src/components/carerpage/SignIn.jsx
@@ -44,7 +44,7 @@ function SignIn() {
                 {errorState ? 'Invalid login details' : ''}
               </p>
               <div className="form-group text-color">
-                <label htmlFor="exampleInputTitle form__label">Emails</label>
+                <label htmlFor="exampleInputTitle form__label">Email</label>
                 <input
                   type="email"
                   className="form-control form-control-lg  form__input "
